d77e52c2af23ae168e41bf571e0bceb73ceafa67
Pegue Judicael OUEDRAOGO <pegueo2000@gmail.com>
2024-04-11 00:52:09 +0000

    firebase 9 configuration

---
+++ src/components/Firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const config = {
+    apiKey: "AIzaSyD1Noukl1eb2YCCAlVOaf948AA4ZnzTPBA",
+    authDomain: "marvel-quiz-3a3cf.firebaseapp.com",
+    projectId: "marvel-quiz-3a3cf",
+    storageBucket: "marvel-quiz-3a3cf.appspot.com",
+    messagingSenderId: "872328313529",
+    appId: "1:872328313529:web:4ed2f8b38b2e6a2c8cb968"
+};
+
+const app =initializeApp(config)
