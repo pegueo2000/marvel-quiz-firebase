76fb8d6612334a9145cd48703b591161122a3fae
Pegue Judicael OUEDRAOGO <pegueo2000@gmail.com>
2024-04-11 00:32:35 +0000

    firebase configuration

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,15 @@
+import app from "firebase/app";
+const config = {
+    apiKey: "AIzaSyD1Noukl1eb2YCCAlVOaf948AA4ZnzTPBA",
+    authDomain: "marvel-quiz-3a3cf.firebaseapp.com",
+    projectId: "marvel-quiz-3a3cf",
+    storageBucket: "marvel-quiz-3a3cf.appspot.com",
+    messagingSenderId: "872328313529",
+    appId: "1:872328313529:web:4ed2f8b38b2e6a2c8cb968"
+};
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+    }
+}
+export default Firebase
